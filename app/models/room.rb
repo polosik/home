5c833aaf1886c4cffc4c848858df1eaeb70fe7ae
Marek Kowalczyk <marek-kowalczyk2@wp.pl>
2014-12-14 16:56:42 +0100

    Seeds updated

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ActiveRecord::Base
+
+	has_many :room_categories
+	has_many :categories, through: :room_categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   devise_for :users
   scope '/admin' do
   resources :devices
-
   resources :room_categories
-
   resources :categories
-
   resources :rooms
 end
   get 'main/index'
 
   root 'main#index'
+
+   get '/room/:id', to: 'main#room', as: 'main_room' 
+   get '/category/:id', to: 'main#category', as: 'main_category' 
 
 end
